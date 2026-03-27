37b9109466757cc7f96d98339bff0df9cb620b54
YOUR NAME <directors@crescentcare.co.uk>
2026-03-27 16:18:32 +0000

    Create staff-admin test route

---
+++ app/staff-admin/page.tsx
@@ -0,0 +1,8 @@
+export default function StaffAdminPage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Staff Admin</h1>
+      <p>Admin route is working.</p>
+    </main>
+  );
+}
